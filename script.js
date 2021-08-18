9c5ec0bd75cbd224709ff4b6bdbf8940860d9bc4
Leong <stapler_1@hotmail.com>
2021-08-18 12:56:39 +0800

    Added function to fetch weather from API to HTML

--- script.js
+++ script.js
@@ -1,38 +1,56 @@
 const form = document.querySelector('form');
 form.addEventListener('submit', handleSubmit);
 const btnConvert = document.querySelector('button');
+let fahrenheit = false;
 /* btnConvert.addEventListener('click', convertUnit); */
 
 async function fetchWeatherData(city){
 
-    try {
+
         const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c153617cf834f19994f86b76441f26ed`);
-        const forecast = await request.json();
+        if(request.ok){
+            const data = await request.json();
+            result = data;
+            getLocation();
+            getWeather().then((img) => {
+                const image = document.querySelector('.desc img');
+                image.src = img.url;
+            });
+        } else {
+            console.log('error');
+        }
+}
 
-        return forecast;
-    } catch(err) {
-        console.log(err);
+function clearSearch(){
+    document.querySelector('#search-city').value = '';
+}
+
+async function getWeather(){
+    const weatherList = {
+        'clear sky' : '01d',
+        'few clouds' : '02d',
+        'scattered clouds' : '03d',
+        'broken clouds' : '04d',
+        'shower rain' : '09d',
+        'rain' : '10d',
+        'thunderstorm' : '11d',
+        'snow' : '13d',
+        'mist' : '50d'
     }
-    
-}
+    const weather = result.weather[0].description;
 
-function processData(){
-    let city = document.querySelector('#location').value;
-    document.querySelector('#location').value = '';
-    fetchWeatherData(city).then((res) => {
-        result = res;
-    })
-}
+    const desc = document.querySelector('.desc h2');
+    desc.textContent = weather;
+    const imgCode = weatherList[weather];
+    const image = await fetch(`http://openweathermap.org/img/wn/${imgCode}@2x.png`);
 
-function getWeather(){
-    return result.weather[0].description;
+    return image;
 }
 
 function getLocation(){
-    return `${result.name}, ${result.sys.country}`
+    const location = document.querySelector('.location');
+    location.textContent = `${result.name}, ${result.sys.country}`;
 }
-
-let fahrenheit = false;
 
 function getTemp(){
     return convertTemp(result.main.temp);
@@ -40,6 +58,10 @@ function getTemp(){
 
 function getFeelsLike(){
     return convertTemp(result.main.feels_like);
+}
+
+function getHumidity(){
+    return `${result.main.humidity}%`;
 }
 
 function convertTemp(kelvin){
@@ -50,18 +72,17 @@ function convertTemp(kelvin){
     return `${Math.round(celsius * 9/5 + 32)} °F`;
 }
 
-function getHumidity(){
-    return `${result.main.humidity}%`;
-}
-
 function convertUnit(){
     fahrenheit = !fahrenheit;
 }
 
 function handleSubmit(e){
+    const city = document.querySelector('#search-city').value;
+    clearSearch();
     e.preventDefault();
-    processData()
+    fetchWeatherData(city);
 }
 
 let result;
 
+fetchWeatherData('Kuala Lumpur');
